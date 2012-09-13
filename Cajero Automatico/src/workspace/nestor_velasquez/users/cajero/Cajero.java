7759fa9cec6b82f5c1fa9764f6b467d7f9857d39
Nestor_Velasquez <Nestor_Velasquez@operamobile04.tss2.gov.do>
2012-09-13 12:36:30 -0400

    Nuevo projecto para el cajero creado

---
+++ Cajero Automatico/src/workspace/nestor_velasquez/users/cajero/Cajero.java
@@ -0,0 +1,13 @@
+package workspace.nestor_velasquez.users.cajero;
+
+public class Cajero {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
